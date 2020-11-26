6627a4c337b766de39fc7deee5058386af7caf59
arturradiuk <arturradiuk@gmail.com>
2020-11-26 18:20:46 +0200

    Order is almost done.

--- src/main/java/model/entities/Order.java
+++ src/main/java/model/entities/Order.java
@@ -1,10 +1,34 @@
 package model.entities;
 
+import controller.exceptions.ClientException;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 public class Order {
     @Getter
-    private UUID uuid;
+    private UUID uuid = UUID.randomUUID();
+    @Getter
+    private LocalDateTime orderDateTime;
+    @Getter
+    private List<Good> goods;
+    @Getter
+    private Client client;
+
+    public Order() {
+    }
+
+    public Order(LocalDateTime orderDateTime, List<Good> goods, Client client) throws ClientException {
+        if(orderDateTime == null || goods == null || client == null){
+            throw new ClientException("");
+        }
+        this.orderDateTime = orderDateTime;
+        this.goods = goods;
+        this.client = client;
+    }
+
+
+
 }
