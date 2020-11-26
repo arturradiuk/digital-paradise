2d9f98efe682e1e1e1f5cb8fabf68010dce8ab1a
224293 <224293@edu.p.lodz.pl>
2020-11-26 18:00:57 +0100

    Address finished

--- src/main/java/model/entities/Address.java
+++ src/main/java/model/entities/Address.java
@@ -1,7 +1,28 @@
 package model.entities;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.UUID;
 
 public class Address {
 
+    @Getter @Setter
+    private String street;
+
+    @Getter @Setter
+    private String number;
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "street='" + street + '\'' +
+                ", number='" + number + '\'' +
+                '}';
+    }
+
+    public Address(String street, String number) {
+        this.street = street;
+        this.number = number;
+    }
 }
--- src/main/java/model/entities/Client.java
+++ src/main/java/model/entities/Client.java
@@ -1,10 +1,15 @@
 package model.entities;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.UUID;
 
 public class Client {
     @Getter
     private UUID uuid;
+
+   
+
+
 }
