12a8205a80647d70e79b21f8e71fdf1e3660f306
224293 <224293@edu.p.lodz.pl>
2020-11-26 18:06:39 +0100

    Client finished

--- src/main/java/model/entities/Client.java
+++ src/main/java/model/entities/Client.java
@@ -9,7 +9,28 @@ public class Client {
     @Getter
     private UUID uuid;
 
-   
+    @Getter @Setter
+    private String firstName;
 
+    @Getter @Setter
+    private String lastName;
 
+    @Getter @Setter
+    private Address address;
+
+    public Client(UUID uuid, String firstName, String lastName, Address address) {
+        this.uuid = uuid;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "uuid=" + uuid +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
 }
