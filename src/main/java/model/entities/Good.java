b5e2edb60817c59a38d79c3f26cee95e1b2beee6
224301 Graczyk Bartłomiej <224301@edu.p.lodz.pl>
2020-11-26 15:52:32 +0100

    Good class implemented.

--- src/main/java/model/entities/Good.java
+++ src/main/java/model/entities/Good.java
@@ -1,10 +1,22 @@
 package model.entities;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.UUID;
 
 public class Good {
     @Getter
     private UUID uuid;
+    @Getter @Setter
+    private double price;
+
+    public Good() {
+        this.uuid = UUID.randomUUID();
+    }
+
+    public Good(double price) {
+        this.uuid = UUID.randomUUID();
+        this.price = price;
+    }
 }
