e9ba372d7b9368999de1bc497daa27a8105dc46b
arturradiuk <arturradiuk@gmail.com>
2020-12-25 20:59:45 +0200

    authentication has been added

---
+++ src/main/java/web/IdentityUtils.java
@@ -0,0 +1,30 @@
+package web;
+
+import javax.ejb.ApplicationException;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+@Named
+@ApplicationScoped
+public class IdentityUtils {
+    @Inject
+    HttpServletRequest request;
+
+    public String getMyLogin() {
+        return ((null == request.getUserPrincipal()) ? "" : request.getUserPrincipal().getName());
+//        return request.isUserInRole("ADMINS")+"";
+    }
+
+    public void logOut() {
+        try {
+            request.logout();
+        } catch (ServletException e) {
+            e.printStackTrace();
+        }
+        request.getSession().invalidate();
+
+    }
+}
