91b4928817a7fc9999ceb773054056544cd68dc2
224301 Graczyk Bartłomiej <224301@edu.p.lodz.pl>
2020-12-28 17:03:33 +0100

    New login page was added.

--- src/main/java/web/IdentityUtils.java
+++ src/main/java/web/IdentityUtils.java
@@ -2,16 +2,40 @@ package web;
 
 import javax.ejb.ApplicationException;
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import lombok.Getter;
+import lombok.Setter;
 
 @Named
-@ApplicationScoped
-public class IdentityUtils {
+@SessionScoped
+public class IdentityUtils implements Serializable {
     @Inject
     HttpServletRequest request;
+    
+    @NotNull
+    @Getter
+    @Setter
+    private String username;
+    
+    @NotNull
+    @Getter
+    @Setter
+    private String password;
+    
+    public String login() {
+        try {
+            request.login(username, password);
+            return "Index";
+        } catch (ServletException e) {
+            return "LoginErrorPage";
+        }
+    }
 
     public String getMyLogin() {
         return ((null == request.getUserPrincipal()) ? "" : request.getUserPrincipal().getName());
