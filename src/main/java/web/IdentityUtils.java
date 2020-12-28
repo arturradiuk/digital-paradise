4e03ae4a66e1fc2abdefff465b9a0cc99e8c8cd4
224301 Graczyk Bartłomiej <224301@edu.p.lodz.pl>
2020-12-28 19:11:38 +0100

    Logger added.

--- src/main/java/web/IdentityUtils.java
+++ src/main/java/web/IdentityUtils.java
@@ -1,7 +1,5 @@
 package web;
 
-import javax.ejb.ApplicationException;
-import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -9,6 +7,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -28,11 +28,15 @@ public class IdentityUtils implements Serializable {
     @Setter
     private String password;
     
+    private final Logger logger  = Logger.getLogger(IdentityUtils.class.getName());
+    
     public String login() {
         try {
             request.login(username, password);
+            logger.log(Level.INFO, "Login succeeded.");
             return "Index";
         } catch (ServletException e) {
+            logger.log(Level.WARNING, "Login failed");
             return "LoginErrorPage";
         }
     }
